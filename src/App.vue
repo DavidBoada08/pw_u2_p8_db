b1b5599b395b554a261bd909525bdb8bfdade877
DavidBoada08 <dfboadaa@uce.edu.ec>
2025-05-14 19:43:35 -0500

    Taller13

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <ContadorView />
+  </div>
+</template>
+
+<script>
+import HolaMundoVue from "@/view/HolaMundoVue.vue";
+import ContadorView from "@/view/ContadorView.vue";
+
+export default {
+  name: "App",
+  components: {
+    HolaMundoVue,
+    ContadorView,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>Intalacion de Herramientas Vue, Vetur para Progra Web</p>
+    <p>David Boada</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
